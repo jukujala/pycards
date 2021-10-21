f3c1da4e72526cde26662d9711e0386d957d618f
Jussi Kujala <jussikuj@unity3d.com>
2021-10-22 00:33:38 +0300

    Fix requirements, remove old files.

--- src/battles_in_antiquity/make_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-python generate_card_images.py
-
-python -m tts_utils.create_tts_deck \
-    --input data/card_images \
-    --back data/card_back.png \
-    --output data/deck_templates/
